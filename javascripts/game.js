91c038ac8fdecd1f4fcf0e7b6410442c3332c303
Dan Simon <dan@pipeline.com>
2018-01-20 11:58:21 -0500

    made it possible to do challenge 12 without 1M IP

--- javascripts/game.js
+++ javascripts/game.js
@@ -2473,7 +2473,7 @@ function getPerTickspeedBoost () {
 }
 
 function alterByCorruption (result, cost) {
-  return Math.pow(result, Math.min(1, player.corruptionStart.log2() / cost.log2(), 1.1));
+  return Math.pow(result, Math.min(1, player.corruptionStart.log2() / cost.log2()));
 }
 
 function getTickSpeedMultiplier (cost) {
